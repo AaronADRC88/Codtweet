76f308084459473493c4aafa7be28b8b461b87b6
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-03-09 10:24:18 +0100

    programa twitter

--- src/tweetcod2015/TweetCOD2015.java
+++ src/tweetcod2015/TweetCOD2015.java
@@ -1,48 +1,35 @@
 package tweetcod2015;
 
 import javax.swing.JOptionPane;
-import twitter4j.DirectMessage;
-import twitter4j.Status;
-import twitter4j.Twitter;
+
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+
 
 public class TweetCOD2015 {
 
     public static void main(String[] args) throws TwitterException {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("d4ZLnromJ6t3192RLDTGIlAqt")
-                .setOAuthConsumerSecret("F6Ouctu4HiYSAouDRrIX7T8POlxYg33W8cgDK9hRn9V6OBqMN1")
-                .setOAuthAccessToken("714352070-MQTk2JtlQ5ft6ZAWuIO2cezy1Rj9p9Dp3LZ6Smz1")
-                .setOAuthAccessTokenSecret("sXZ9RxKBoWev2g12mrsOPnAf4yNweoPatCSHcr6Hrrjym");
-        /**
-         * Escribir tweet Twitter twitter = new
-         * TwitterFactory(cb.build()).getInstance();
-         *
-         * String tweet=JOptionPane.showInputDialog(null, "Escribe tu tweet",
-         * "Inserta 140 caracteres", JOptionPane.QUESTION_MESSAGE); Status
-         * status = twitter.updateStatus(tweet); System.out.println("Success" +
-         * status.getText());
-    }*
-         */
-        /**mandar mensaje directo yusuke code
-             if (args.length < 2) {
-            System.out.println("Usage: java twitter4j.examples.directmessage.SendDirectMessage [recipient screen name] [message]");
-            System.exit(-1);
-        }
-        Twitter twitter = new TwitterFactory().getInstance();
-        try {
-            DirectMessage message = twitter.sendDirectMessage(args[0], args[1]);
-            System.out.println("Direct message successfully sent to " + message.getRecipientScreenName());
-            System.exit(0);
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to send a direct message: " + te.getMessage());
-            System.exit(-1);
-        }
-
-    }**/
+// TODO code application logic here
+Metodos twit = new Metodos();
+int op;
+do {
+op = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> Exit"));
+switch (op) {
+case 1:
+twit.timeLine();
+break;
+case 2:
+twit.buscar();
+break;
+case 3:
+twit.tweet();
+break;
+case 4:
+System.exit(0);
+default:
+System.exit(0);
 }
+} while (op != 0 && op != 4);
+}
+        
+
 }
