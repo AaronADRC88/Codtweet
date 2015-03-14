0647bb439edf1e749d74cb4de377f96e984105e0
Layun <Layun@Layun-PC>
2015-03-14 16:21:01 +0100

    menu heccho problemas reparados

--- src/tweetcod2015/TweetCOD2015.java
+++ src/tweetcod2015/TweetCOD2015.java
@@ -4,32 +4,30 @@ import javax.swing.JOptionPane;
 
 import twitter4j.TwitterException;
 
-
 public class TweetCOD2015 {
 
     public static void main(String[] args) throws TwitterException {
-// TODO code application logic here
-Metodos twit = new Metodos();
-int op;
-do {
-op = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> Exit"));
-switch (op) {
-case 1:
-twit.timeLine();
-break;
-case 2:
-twit.buscar();
-break;
-case 3:
-twit.tweet();
-break;
-case 4:
-System.exit(0);
-default:
-System.exit(0);
-}
-} while (op != 0 && op != 4);
-}
-        
+        /**
+         * instancia objeto tipo Metodos()
+         * Ventana para acceder a metodos
+         */
+        Metodos tweet = new Metodos();
+        int option = JOptionPane.showOptionDialog(null, "selecciona opcion", "menu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Mostrar TimeLine", "Buscar en Twitter", "Publica un tweet", "Mandar mensaje a: ", "Salir"}, "Salir");
+        if (option == 0) {
+            tweet.timeLine();
+        }
+        if (option == 1) {
+            tweet.buscar();
+        }
+        if (option == 2) {
+            tweet.tweet();
+        }
+        if (option == 3) {
+            tweet.directMsg();
+        } else if (option != 0 && option != 1 && option != 2 && option != 3) {
+            System.exit(0);
+        }
+
+    }
 
 }
